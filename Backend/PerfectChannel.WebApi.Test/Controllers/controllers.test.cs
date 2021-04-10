a50cf7d278db00d9a19b8e668009b0d922afe384
Francisco <javiermartincuenca@gmail.com>
2021-04-10 20:55:37 +0200

    Setupt for backend testing

--- Backend/PerfectChannel.WebApi.Test/Controllers/controllers.test.cs
+++ Backend/PerfectChannel.WebApi.Test/Controllers/controllers.test.cs
@@ -1,1 +1,26 @@
 // Add your code here and rename the file accordingly.
+
+using NUnit.Framework;
+using PerfectChannel.WebApi.Controllers;
+
+namespace PerfectChannel.WebApi.Test.Controllers
+{
+
+  [TestFixture]
+  public class TodoServiceTest
+  {
+    private TaskController _todoService;
+
+    [SetUp]
+    public void SetUp()
+    {
+      _todoService = new TaskController();
+    }
+
+    [Test]
+    public void ReturnFalse()
+    {
+      Assert.IsFalse(false);
+    }
+  }
+}
