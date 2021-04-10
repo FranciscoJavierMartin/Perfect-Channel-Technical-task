20dea54d103ef358e334f631cafcfdd2d300f6b7
Francisco <javiermartincuenca@gmail.com>
2021-04-10 10:42:49 +0200

    Initial setup

---
+++ Backend/PerfectChannel.WebApi/Controllers/TaskController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PerfectChannel.WebApi.Controllers
+{
+  [Route("api/[controller]")]
+  [EnableCors("AllowOrigin")]
+  [ApiController]
+  public class TaskController : ControllerBase
+  {
+
+    public async Task<ActionResult<string[]>> GetTodos()
+    {
+      return new string[1] { "Hello" };
+    }
+  }
+}
