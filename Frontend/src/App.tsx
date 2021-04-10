de74d81f077fd937937f3bd49f0b381d2eb318ec
Francisco <javiermartincuenca@gmail.com>
2021-04-10 18:53:06 +0200

    Store todos

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Todo } from './models/todo';
+import { getCompletedTodos, getPendingTodos } from './network/todos';
 import './App.css';
-import { getCompletedTodos } from './network/todos';
 
 const App: React.FC = () => {
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
+  const [pendingTodos, setPendingTodos] = useState<Todo[]>([]);
+
   useEffect(() => {
-    getCompletedTodos().then(console.log);
+    getCompletedTodos().then((res) => {
+      setCompletedTodos(res);
+    });
+    getPendingTodos().then((res) => {
+      setPendingTodos(res);
+    });
   });
-  return <div></div>;
+  return <div>
+    
+  </div>;
 };
 
 export default App;
