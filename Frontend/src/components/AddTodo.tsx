4ea8979522407f708e4de1781f13dffba1b6b70c
Francisco <javiermartincuenca@gmail.com>
2021-04-11 21:39:02 +0200

    Add margin to input

--- Frontend/src/components/AddTodo.tsx
+++ Frontend/src/components/AddTodo.tsx
@@ -32,7 +32,7 @@ const AddTodo: React.FC<AddTodoProps> = () => {
   };
 
   return (
-    <div className='row justify-content-between text-white mb-3'>
+    <div className='row justify-content-between text-white mb-3 ml-1 mr-1'>
       <form onSubmit={submitHandler} className='input-group'>
         <input
           type='text'
